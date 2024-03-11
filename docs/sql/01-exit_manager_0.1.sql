7ad85366c3851aca3b18400150b3703cd2978942
Armando Pagliara <armpag@gmail.com>
2024-03-11 10:40:19 +0100

    upgrade

--- docs/sql/01-exit_manager_0.1.sql
+++ docs/sql/01-exit_manager_0.1.sql
@@ -1,3 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Creato il: Gen 25, 2024 alle 13:37
+-- Versione del server: 10.1.13-MariaDB
+-- Versione PHP: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `exitman`
+--
+CREATE DATABASE IF NOT EXISTS `exitman`;  
+
+USE `exitman`;
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `classe`
+--
+
+CREATE TABLE `classe` (
+  `codice_classe` varchar(8) NOT NULL,
+  `nome_classe` varchar(4) NOT NULL,
+  `anno_scolastico` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `docente`
+--
+
 CREATE TABLE `docente` (
   `codice_Docente` varchar(6) PRIMARY KEY NOT NULL,
   `nome` varchar(255) NOT NULL,
@@ -6,3 +48,20 @@ CREATE TABLE `docente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `docente` (`codice_Docente`, `nome`, `cognome`, `password`) VALUES ('AA001', 'Armando', 'Pagliara', 'armpag83');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `classe`
+--
+ALTER TABLE `classe`
+  ADD PRIMARY KEY (`codice_classe`);
+
+--
+-- Indici per le tabelle `docente`
+--
+ALTER TABLE `docente`
+  ADD PRIMARY KEY (`codice_Docente`);
+
